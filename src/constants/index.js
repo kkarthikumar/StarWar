01b5be2eeb9d713c18f973d2a4ca71af473ab089
kkarthikumar <venk71@sg.uob>
2019-10-06 15:28:13 +0800

    updated no of searches

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,5 @@ export const BASE_URL = "https://swapi.co/api";
 export const PLANETS_API_URL = "/planets/";
 export const USER_API_URL = "/people/";
 
-export const VALID_UNTIL = 20 * 1000;
-export const NUMBER_OF_SEARCHES = 3;
+export const VALID_UNTIL = 60 * 1000;
+export const NUMBER_OF_SEARCHES = 15;
