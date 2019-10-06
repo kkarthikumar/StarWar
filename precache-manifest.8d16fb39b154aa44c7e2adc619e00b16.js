e2980f4bfd8a1c6d533bd0ae40ee630a7988acf3
kkarthikumar <venk71@sg.uob>
2019-10-06 15:39:15 +0800

    Updates

---
+++ precache-manifest.8d16fb39b154aa44c7e2adc619e00b16.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0a41ccb5d10d5ca5d99e5ce6fa67aa",
+    "url": "/kkarthikumar/StarWar/gh-page-starwar/index.html"
+  },
+  {
+    "revision": "42bde1c1777fa9a08853",
+    "url": "/kkarthikumar/StarWar/gh-page-starwar/static/js/2.d982be22.chunk.js"
+  },
+  {
+    "revision": "ec1d9549a9b76b85e2d7",
+    "url": "/kkarthikumar/StarWar/gh-page-starwar/static/js/main.c8e76c75.chunk.js"
+  },
+  {
+    "revision": "c0c4814e0ad143d0e9cf",
+    "url": "/kkarthikumar/StarWar/gh-page-starwar/static/js/runtime-main.30cbf093.js"
+  }
+]);
